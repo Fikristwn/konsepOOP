a9b5f253095b534aff4ee2ceff0309a00c2ba266
WIN 10 <setiawanmfikri@gmail.com>
2024-09-17 11:11:27 +0700

    stater project

---
+++ src/main/kotlin/personD.kt
@@ -0,0 +1,9 @@
+class PersonD {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    constructor(_firstName: String, _lastName: String, _age: Int){
+        firstName = _firstName
+        lastName = _lastName
+        age = _age
+    }}
